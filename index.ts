be616f99e50053ba28e611d97b6bd335592a09e5
Anson Chung <ansonchung3@gmail.com>
2026-03-28 20:09:35 +0000

    Implement `-h` helper flag

--- index.ts
+++ index.ts
@@ -1,12 +1,14 @@
 #!/usr/bin/env tsx
 import { Command } from 'commander';
-import { registerCommands } from './commands/index';
+import { registerCommands, privateCommandNames } from './commands/index';
 import { registerBootAction } from './internal/boot';
+import { configureHelp } from './internal/help';
 
 const program = new Command();
 program.name('colleague').description('Personal CLI').version('1.0.0');
 
 await registerCommands(program);
 registerBootAction(program);
+configureHelp(program, privateCommandNames);
 
 program.parse();
